c941931a374773f492db25d4bab413e29971b10e
System Administrator <root@JamesDingdeMacBook-Air.local>
2015-11-20 09:15:08 +0800

    1
    1

--- day1/test1.py
+++ day1/test1.py
@@ -1,3 +1,22 @@
-__author__ = 'root'
 
+#coding=utf-8
 
+print "你好，世界";
+print "你好，世界";
+print "Hello I'm OK"
+print r'\(~_~)/ \(~_~)/'
+print ''' This is a new hang '''
+print (1+2)*3+7.5/3+2.1
+print 1.0+3.0
+a = True
+print a and 'a=T' or 'a=F'
+
+if True:
+    print "Ture"
+else:
+    print "False"
+print "adsfadf"
+#数组
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+print days[1]
