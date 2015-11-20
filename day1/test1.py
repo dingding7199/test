6b4dbce572f2d8122eae872f37b1f3e84b631071
System Administrator <root@JamesDingdeMacBook-Air.local>
2015-11-20 20:37:09 +0800

    1
    1

--- day1/test1.py
+++ day1/test1.py
@@ -1,13 +1,12 @@
-
-#coding=utf-8
+# coding=utf-8
 
 print "你好，世界";
 print "你好，世界";
 print "Hello I'm OK"
 print r'\(~_~)/ \(~_~)/'
 print ''' This is a new hang '''
-print (1+2)*3+7.5/3+2.1
-print 1.0+3.0
+print (1 + 2) * 3 + 7.5 / 3 + 2.1
+print 1.0 + 3.0
 a = True
 print a and 'a=T' or 'a=F'
 
@@ -19,4 +18,286 @@ print "adsfadf"
 #数组
 days = ['Monday', 'Tuesday', 'Wednesday',
         'Thursday', 'Friday']
-print days[1]
+#等待客户输入
+'''
+raw_input("\n\nPress the enter key to exit.")
+print raw_input();
+'''
+import sys;
+
+x = 'foo';
+sys.stdout.write(x + '\n')
+str = 'Hello World!'
+#字符串
+print str  # 输出完整字符串
+print str[0]  # 输出字符串中的第一个字符
+print str[2:5]  # 输出字符串中第三个至第五个之间的字符串
+print str[2:]  # 输出从第三个字符开始的字符串
+print str * 2  # 输出字符串两次
+print str + "TEST"  # 输出连接的字符串
+
+#元素
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+print list # 输出完整列表
+print list[0] # 输出列表的第一个元素
+print list[1:3] # 输出第二个至第三个的元素
+print list[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2 # 输出列表两次
+print list + tinylist # 打印组合的列表
+
+#元组
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+tinytuple = (123, 'john')
+
+print tuple # 输出完整元组
+print tuple[0] # 输出元组的第一个元素
+print tuple[1:3] # 输出第二个至第三个的元素
+print tuple[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinytuple * 2 # 输出元组两次
+print tuple + tinytuple # 打印组合的元组
+
+'''
+以下是元组无效的，因为元组是不允许更新的。而列表是允许更新的：
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tuple[2] = 1000 # 元组中是非法应用
+list[2] = 1000 # 列表中是合法应用
+'''
+'''
+字典(dictionary)是除列表以外python之中最灵活的内置数据结构类型。列表是有序的对象结合，字典是无序的对象集合。
+两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+字典用"{ }"标识。字典由索引(key)和它对应的值value组成。
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print dict['one'] # 输出键为'one' 的值
+print dict[2] # 输出键为 2 的值
+print tinydict # 输出完整的字典
+print tinydict.keys() # 输出所有键
+print tinydict.values() # 输出所有值
+print tinydict['name']
+#算术运算符
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print "Line 1 - Value of c is ", c
+
+c = a - b
+print "Line 2 - Value of c is ", c
+
+c = a * b
+print "Line 3 - Value of c is ", c
+
+c = a / b
+print "Line 4 - Value of c is ", c
+
+c = a % b
+print "Line 5 - Value of c is ", c
+
+a = 2
+b = 3
+c = a**b
+print "Line 6 - Value of c is ", c
+
+a = 10
+b = 5
+c = a//b
+print "Line 7 - Value of c is ", c
+
+#比较运算符
+a = 21
+b = 10
+c = 0
+
+if ( a == b ):
+   print "Line 1 - a is equal to b"
+else:
+   print "Line 1 - a is not equal to b"
+
+if ( a != b ):
+   print "Line 2 - a is not equal to b"
+else:
+   print "Line 2 - a is equal to b"
+
+if ( a <> b ):
+   print "Line 3 - a is not equal to b"
+else:
+   print "Line 3 - a is equal to b"
+
+if ( a < b ):
+   print "Line 4 - a is less than b"
+else:
+   print "Line 4 - a is not less than b"
+
+if ( a > b ):
+   print "Line 5 - a is greater than b"
+else:
+   print "Line 5 - a is not greater than b"
+
+a = 5;
+b = 20;
+if ( a <= b ):
+   print "Line 6 - a is either less than or equal to  b"
+else:
+   print "Line 6 - a is neither less than nor equal to  b"
+
+if ( b >= a ):
+   print "Line 7 - b is either greater than  or equal to b"
+else:
+   print "Line 7 - b is neither greater than  nor equal to b"
+
+#赋值运算符
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print "Line 1 - Value of c is ", c
+
+c += a
+print "Line 2 - Value of c is ", c
+
+c *= a
+print "Line 3 - Value of c is ", c
+
+c /= a
+print "Line 4 - Value of c is ", c
+
+c  = 2
+c %= a
+print "Line 5 - Value of c is ", c
+
+c **= a
+print "Line 6 - Value of c is ", c
+
+c //= a
+print "Line 7 - Value of c is ", c
+
+#逻辑运算符
+a = 10
+b = 20
+c = 0
+
+if ( a and b ):
+   print "Line 1 - a and b are true"
+else:
+   print "Line 1 - Either a is not true or b is not true"
+
+if ( a or b ):
+   print "Line 2 - Either a is true or b is true or both are true"
+else:
+   print "Line 2 - Neither a is true nor b is true"
+
+
+a = 0
+if ( a and b ):
+   print "Line 3 - a and b are true"
+else:
+   print "Line 3 - Either a is not true or b is not true"
+
+if ( a or b ):
+   print "Line 4 - Either a is true or b is true or both are true"
+else:
+   print "Line 4 - Neither a is true nor b is true"
+
+if not( a and b ):
+   print "Line 5 - Either a is not true or b is  not true or both are not true"
+else:
+   print "Line 5 - a and b are true"
+
+
+#成员运算符；除了以上的一些运算符之外，Python还支持成员运算符，测试实例中包含了一系列的成员，包括字符串，列表或元组
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5 ];
+
+if ( a in list ):
+   print "Line 1 - a is available in the given list"
+else:
+   print "Line 1 - a is not available in the given list"
+
+if ( b not in list ):
+   print "Line 2 - b is not available in the given list"
+else:
+   print "Line 2 - b is available in the given list"
+
+a = 2
+if ( a in list ):
+   print "Line 3 - a is available in the given list"
+else:
+   print "Line 3 - a is not available in the given list"
+
+#运算符优先级
+a = 20
+b = 20
+
+if ( a is b ):
+   print "Line 1 - a and b have same identity"
+else:
+   print "Line 1 - a and b do not have same identity"
+
+if ( id(a) == id(b) ):
+   print "Line 2 - a and b have same identity"
+else:
+   print "Line 2 - a and b do not have same identity"
+
+b = 30
+if ( a is b ):
+   print "Line 3 - a and b have same identity"
+else:
+   print "Line 3 - a and b do not have same identity"
+
+if ( a is not b ):
+   print "Line 4 - a and b do not have same identity"
+else:
+   print "Line 4 - a and b have same identity"
+
+# 例3：if语句多个条件
+
+num = 9
+if num >= 0 and num <= 10:    # 判断值是否在0~10之间
+    print 'hello'
+#>>> hello		# 输出结果
+
+num = 10
+if num < 0 or num > 10:    # 判断值是否在小于0或大于10
+    print 'hello'
+else:
+	print 'undefine'
+#>>> undefine		# 输出结果
+
+num = 8
+# 判断值是否在0~5或者10~15之间
+if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):
+    print 'hello'
+else:
+    print 'undefine'
+#>>> undefine		# 输出结果
+
+
+#while语句
+count = 0
+while(count<9):
+    print "NUM is count",count
+    count+=1
+print "Bye"
+
+
+
+
+
+
+
+
+
